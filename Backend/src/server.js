c847e797021f46763c1973d1d77c49c490c06fa5
Krushna Raut <krishraut2103@gmail.com>
2025-10-03 15:20:06 +0530

    message route handler

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,13 +3,14 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from './routes/auth.route.js';
-
+import authMessages from './routes/message.route.js';
 
 const PORT = process.env.PORT || 3000 ;
 
 const app = express();
 
 app.use('/api/auth', authRoutes);
+app.use('api/messages', authMessages);
 
 app.listen(PORT ,() => {console.log('Server is running on port: ' + PORT);
 })
