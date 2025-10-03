f115715e1302d3a9dc1e5168ff439c90bc238b8a
Krushna Raut <krishraut2103@gmail.com>
2025-10-03 14:57:18 +0530

    Dependancies

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,7 @@
-import { Server } from "socket.io";     
-import express from 'express';
-import { createServer } from "node:http";
-
+// const express = require('express');
+import express from "express";
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+app.listen(3000 ,() => {console.log('Server is running on port 3000');
+})
