aa71d811bb89484cd710b8fd654186984312a63b
Krushna Raut <krishraut2103@gmail.com>
2025-10-03 15:15:40 +0530

    route Handler

---
+++ Backend/src/server.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import authRoutes from './routes/auth.route.js';
+
+
+const PORT = process.env.PORT || 3000 ;
+
+const app = express();
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT ,() => {console.log('Server is running on port: ' + PORT);
+})
