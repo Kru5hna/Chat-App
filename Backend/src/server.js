792803e1d84224063a7bcffb89806db85668ed44
Krushna Raut <krishraut2103@gmail.com>
2025-10-04 20:42:55 +0530

    Arcjet :  Rate limiting

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -16,6 +16,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// app.use(express.json({limit : '5mb'})); // later in the video
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
