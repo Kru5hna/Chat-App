1e20fad9fc786cc31487a544017eabf708d012f0
Krushna Raut <krishraut2103@gmail.com>
2025-10-05 17:22:21 +0530

    frontend Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
-
+import { ChatPage } from './Pages/ChatPage'
+import { LoginPage } from './Pages/LoginPage'
+import { Route, Routes } from 'react-router' 
+import { SignUpPage } from './Pages/SignupPage'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className='text-green-700 font-bold bg-amber-400'>heyy</h1>
-    </>
-  )
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      {/* DECORATORS - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={ <SignUpPage /> } />
+      </Routes>
+
+      
+    </div>
+  );
 }
 
 export default App
